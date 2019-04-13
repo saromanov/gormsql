c0b07d955c68cbeb2163074100ab6ddbe1f8ab4c
Sergey <xxsmotur@gmail.com>
2019-04-13 20:13:01 +0500

    Reading of data from tables

--- cmd/gormsql/gormsql.go
+++ cmd/gormsql/gormsql.go
@@ -3,15 +3,29 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	"github.com/saromanov/gormsql/pkg/sqlparser"
+	"github.com/saromanov/gormsql/pkg/os"
 )
 
 var (
 	sqlTablesPath = flag.String("word", "foo", "a string")
 )
 
+func createModelFromTables(path string){
+	dat, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
+
+	err = sqlparser.Parse(dat)
+	if err != nil {
+		panic(err)
+	}
+}
 func run(){
 	if *sqlTablesPath != "" {
-
+		createModelFromTables(*sqlTablesPath)
 	}
 }
 func main(){
