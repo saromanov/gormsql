4d21c80cb8cb869807c4baa98db971230412318f
Sergey <xxsmotur@gmail.com>
2019-04-13 23:51:00 +0500

    definition for tables

---
+++ pkg/core/core.go
@@ -0,0 +1,5 @@
+// Package core defines main logic
+package core
+
+type Core struct {
+}
---
+++ pkg/core/table.go
@@ -0,0 +1,10 @@
+package core
+
+// Table represents gorm model definition
+type Table struct {
+	Name    string
+	Columns []Column
+}
+
+type Column struct {
+}
