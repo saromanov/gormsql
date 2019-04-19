c6c8ae89ffd3c4979a89272c17d47dafb984f581
Sergey <xxsmotur@gmail.com>
2019-04-20 01:03:00 +0500

    Add Do method

--- pkg/core/core.go
+++ pkg/core/core.go
@@ -1,6 +1,12 @@
 // Package core defines main logic
 package core
 
+import (
+	"errors"
+)
+
+var errNoName = errors.New("name is not defined")
+
 type Core struct {
 	table Table
 }
@@ -10,4 +16,12 @@ func New(t Table) *Core {
 	return &Core{
 		table: t,
 	}
+}
+
+// Do provides generation of the gorm tables
+func (c *Core) Do() error {
+	if c.table.Name == "" {
+		return errNoName
+	}
+	return nil
 }
